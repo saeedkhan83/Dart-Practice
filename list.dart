fb385a42ed87834111bf95c794682c26d556982b
saeedkhan83 <122628958+saeedkhan83@users.noreply.github.com>
2024-07-03 12:54:54 +0500

    Learn List

---
+++ list.dart
@@ -0,0 +1,8 @@
+void main (){
+  List<String> names = ['Hamid', 'Ahmed', 'Hamad'];
+  print ('Name of the Second Employee '+names[1]);
+  List<String> Depart = ['Accounts', 'Sales', 'HR'];
+  print ('Depart of Employee ' + Depart[1]);
+  print (names[1] + ' Works in the '+ Depart[1]);
+
+}
