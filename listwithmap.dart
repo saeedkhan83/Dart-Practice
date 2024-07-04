3d9ca8976a91a10d1d5134cf1c63cd801d0390cd
saeedkhan83 <122628958+saeedkhan83@users.noreply.github.com>
2024-07-04 12:09:51 +0500

    Learn List with Map

---
+++ listwithmap.dart
@@ -0,0 +1,15 @@
+void main(){
+List<Map<String, String>> emp = [
+  {'Name' : 'Saeed',
+  'Age' : '38',
+  'Department': 'IT',
+  },
+  {'Name': 'Kashif',
+  'Age' : '30',
+  'Department': 'Accounts'},
+  {'Name' : 'Ahmed',
+  'Age': '24',
+  'Deparment': 'HR'},
+];
+print(emp[2]);
+}
